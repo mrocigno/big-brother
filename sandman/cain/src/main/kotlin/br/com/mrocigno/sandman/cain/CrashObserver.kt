303e4a8ab07a1fd0c0f6fc11794c5bed6d8c4045
mrocigno <rocignom@gmail.com>
2023-03-21 11:51:05 -0300

    removendo leak e colocando o bom dentro de core

--- sandman/cain/src/main/kotlin/br/com/mrocigno/sandman/cain/CrashObserver.kt
+++ sandman/cain/src/main/kotlin/br/com/mrocigno/sandman/cain/CrashObserver.kt
@@ -5,13 +5,20 @@ import android.os.Process
 import br.com.mrocigno.sandman.common.utils.printScreen
 import br.com.mrocigno.sandman.common.utils.save
 import br.com.mrocigno.sandman.core.utils.lastClickPosition
+import java.lang.ref.WeakReference
 import kotlin.system.exitProcess
 
 class CrashObserver(
-    private val activity: Activity
+    val activity: WeakReference<Activity>,
+    val default: Thread.UncaughtExceptionHandler?
 ) : Thread.UncaughtExceptionHandler {
 
     override fun uncaughtException(t: Thread, e: Throwable) {
+        val activity = activity.get() ?: run {
+            default?.uncaughtException(t, e)
+            return
+        }
+
         activity
             .printScreen(lastClickPosition)
             .save(activity, "print_crash.png")
--- sandman/cain/src/main/kotlin/br/com/mrocigno/sandman/cain/CrashTask.kt
+++ sandman/cain/src/main/kotlin/br/com/mrocigno/sandman/cain/CrashTask.kt
@@ -1,12 +1,24 @@
 package br.com.mrocigno.sandman.cain
 
 import android.app.Activity
+import java.lang.ref.WeakReference
 
 class CrashTask : br.com.mrocigno.sandman.core.MorpheusTask() {
 
     override fun onActivityStarted(activity: Activity) {
         super.onActivityStarted(activity)
 
-        Thread.setDefaultUncaughtExceptionHandler(CrashObserver(activity))
+        val currentObserver = Thread.getDefaultUncaughtExceptionHandler()
+        val default = if (currentObserver is CrashObserver) {
+            currentObserver.activity.clear()
+            currentObserver.default
+        } else {
+            currentObserver
+        }
+
+        Thread.setDefaultUncaughtExceptionHandler(CrashObserver(
+            WeakReference(activity),
+            default
+        ))
     }
 }
